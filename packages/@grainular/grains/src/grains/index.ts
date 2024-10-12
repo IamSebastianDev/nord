a36fa6b64f6f6026e03c294e72a826827c17d0a1
Sebastian Heinz <sebastian@iamsebastian.dev>
2024-10-12 21:04:25 +0200

    chore: add package exports

---
+++ packages/@grainular/grains/src/grains/index.ts
@@ -0,0 +1,6 @@
+/** @format */
+
+export { combined } from './combined';
+export { grain } from './grain';
+export type { Grain, Subscriber, Unsubscribe, WritableGrain } from './grain';
+export { readonly } from './readonly';
---
+++ packages/@grainular/grains/src/index.ts
@@ -0,0 +1,3 @@
+/** @format */
+export { combined, grain, readonly } from './grains';
+export type { Grain, Subscriber, Unsubscribe, WritableGrain } from './grains';
