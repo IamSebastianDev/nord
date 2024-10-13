2381772a2f6e964d5efd3337e8d341941b43aa6d
Sebastian Heinz <sebastian@iamsebastian.dev>
2024-10-13 11:57:37 +0200

    feat: provide exports for a complete iife bundle

---
+++ packages/@grainular/grains/src/browser.ts
@@ -0,0 +1,6 @@
+/** @format */
+
+export { grain, readonly } from './index';
+export type { Grain, Subscriber, Unsubscribe, WritableGrain } from './index';
+export { map, pipe, tap } from './operators';
+export type { Operator } from './operators';
